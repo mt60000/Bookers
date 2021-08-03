6f6cf13321bfc8c5b654ffe16cad8df481289cff
mt60000 <niruyaku@gmail.com>
2021-08-03 04:33:51 +0000

    destroyの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,43 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new(params[:id])
+  end
+
+  def create
+    @book = Book.new(books_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book =Book.find(params[:id])
+    book.update(book_params)
+    redirect_to books_path
+  end
+
+  def destroy
+    book =Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def books_params
+    params.permit(:title, :body)
   end
 end
